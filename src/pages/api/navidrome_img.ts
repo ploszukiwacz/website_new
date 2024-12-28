3e40ba193c25e50147d3267bf3a4fc5d7b41fb0c
PlOszukiwacz <87446593+PlOszukiwaczDEV@users.noreply.github.com>
2024-12-28 16:55:06 +0100

    more vrc images & api update

--- src/pages/api/navidrome_img.ts
+++ src/pages/api/navidrome_img.ts
@@ -1,17 +1,6 @@
-import type { APIRoute } from 'astro';
-
 export const GET: APIRoute = async () => {
   try {
-    const images = [
-      'vrc1.png',
-      'vrc2.png',
-      'vrc3.png',
-      'vrc4.png',
-      'vrc5.png',
-      'vrc6.png',
-      'vrc7.png',
-      'vrc8.png'
-    ];
+    const images = Array.from({ length: 15 }, (_, i) => `vrc${i + 1}.png`);
 
     const randomImage = images[Math.floor(Math.random() * images.length)];
     const imagePath = `/vrc/${randomImage}`;
@@ -24,7 +13,6 @@ export const GET: APIRoute = async () => {
       }
     });
   } catch (error) {
-    console.error('Error:', error);
     return new Response('Error', { status: 500 });
   }
 };
