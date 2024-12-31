ce04455379044e7d45de59d04cf53ebdfedd1879
PlOszukiwacz <87446593+PlOszukiwaczDEV@users.noreply.github.com>
2024-12-31 21:05:42 +0100

    made the website be catppuccin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,42 @@
+// import defaultTheme from "tailwindcss/defaultTheme";
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   darkMode: ["class"],
+//   content: [
+//     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+//         serif: ["Lora", ...defaultTheme.fontFamily.serif],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/typography")],
+// };
+
+
 import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: ["Lora", ...defaultTheme.fontFamily.serif],
       },
+      colors: {
+        mocha: {
+          base: '#1e1e2e',
+          mantle: '#181825',
+          text: '#cdd6f4'
+        }
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
